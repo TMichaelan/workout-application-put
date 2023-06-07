13a4baf65f21226eb84b37f61ae8cdda7c15e5b5
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2023-06-07 11:56:29 +0200

    add-recyclerview

--- app/src/main/java/com/example/mobile_applications_project_put/adapters/BodyPartAdapter.kt
+++ app/src/main/java/com/example/mobile_applications_project_put/adapters/BodyPartAdapter.kt
@@ -3,44 +3,45 @@ package com.example.mobile_applications_project_put.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobile_applications_project_put.R
-import com.example.mobile_applications_project_put.databinding.BodyPartsBinding
+import com.example.mobile_applications_project_put.models.BodyPartsList
 
-class BodyPartAdapter: RecyclerView.Adapter<BodyPartAdapter.BodyPartsViewHolder>() {
-    private var bodyPartsList = ArrayList<String>()
+class BodyPartAdapter(
+    private val bodyPartsList: ArrayList<BodyPartsList>,
+    private val listener: OnItemClickListener
+    ): RecyclerView.Adapter<BodyPartAdapter.MyViewHolder>() {
 
 
-    fun setBodyPart(bodyPartsList:ArrayList<String>){
-        this.bodyPartsList = bodyPartsList
-        notifyDataSetChanged()
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BodyPartsViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.body_parts, parent, false)
-        return MyViewHolder(itemView, listener, meallist)
+        return MyViewHolder(itemView, listener, bodyPartsList)
     }
-
-    override fun onBindViewHolder(holder: BodyPartsViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return bodyPartsList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentMuscle = bodyPartsList[position]
+//        Glide.with(holder.imageId)
+//            .load(currentMuscle.gifUrl)
+//            .into(holder.imageId)
+//        holder.name.text = currentMuscle.name
+        holder.itemView.setOnClickListener { listener.onItemClick(currentMuscle) }
     }
 
     interface OnItemClickListener {
-        fun onItemClick(muscle: Class)
+        fun onItemClick(muscle: BodyPartsList)
     }
+
 
     class MyViewHolder(
         itemView: View,
-        private val listener: AdapterView.OnItemClickListener,
-        private val bodyPartsList: ArrayList<String>
+        private val listener: OnItemClickListener,
+        private val bodyPartsList: ArrayList<BodyPartsList>
     ) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val imageId: ImageView = itemView.findViewById(R.id.img_body_part)
         val name: TextView = itemView.findViewById(R.id.tv_body_part)
@@ -53,5 +54,4 @@ class BodyPartAdapter: RecyclerView.Adapter<BodyPartAdapter.BodyPartsViewHolder>
             listener.onItemClick(bodyPartsList[adapterPosition])
         }
     }
-    class BodyPartsViewHolder(val binding: BodyPartsBinding): RecyclerView.ViewHolder(binding.root)
 }
