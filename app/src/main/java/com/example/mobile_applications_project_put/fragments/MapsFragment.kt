5f3b6518799ec9ee4d2cb98ace773ea70bf8641a
TMichael <bigapplematthias@gmail.com>
2023-06-07 11:10:01 +0200

    comment maps

--- app/src/main/java/com/example/mobile_applications_project_put/fragments/MapsFragment.kt
+++ app/src/main/java/com/example/mobile_applications_project_put/fragments/MapsFragment.kt
@@ -1,62 +1,62 @@
-package com.example.mobile_applications_project_put.fragments
-
-import androidx.fragment.app.Fragment
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.mobile_applications_project_put.R
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-
-class MapsFragment : Fragment() {
-
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        val mountainView = LatLng(37.4, -122.1)
-
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10f))
-
-
-        // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
-        val cameraPosition = CameraPosition.Builder()
-            .target(mountainView) // Sets the center of the map to Mountain View
-            .zoom(17f)            // Sets the zoom
-            .bearing(90f)         // Sets the orientation of the camera to east
-            .tilt(30f)            // Sets the tilt of the camera to 30 degrees
-            .build()              // Creates a CameraPosition from the builder
-        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
-    }
-}
+//package com.example.mobile_applications_project_put.fragments
+//
+//import androidx.fragment.app.Fragment
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import com.example.mobile_applications_project_put.R
+//
+//import com.google.android.gms.maps.CameraUpdateFactory
+//import com.google.android.gms.maps.GoogleMap
+//import com.google.android.gms.maps.OnMapReadyCallback
+//import com.google.android.gms.maps.SupportMapFragment
+//import com.google.android.gms.maps.model.CameraPosition
+//import com.google.android.gms.maps.model.LatLng
+//import com.google.android.gms.maps.model.MarkerOptions
+//
+//class MapsFragment : Fragment() {
+//
+//    private val callback = OnMapReadyCallback { googleMap ->
+//        /**
+//         * Manipulates the map once available.
+//         * This callback is triggered when the map is ready to be used.
+//         * This is where we can add markers or lines, add listeners or move the camera.
+//         * In this case, we just add a marker near Sydney, Australia.
+//         * If Google Play services is not installed on the device, the user will be prompted to
+//         * install it inside the SupportMapFragment. This method will only be triggered once the
+//         * user has installed Google Play services and returned to the app.
+//         */
+//        val sydney = LatLng(-34.0, 151.0)
+//        val mountainView = LatLng(37.4, -122.1)
+//
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10f))
+//
+//
+//        // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+//        val cameraPosition = CameraPosition.Builder()
+//            .target(mountainView) // Sets the center of the map to Mountain View
+//            .zoom(17f)            // Sets the zoom
+//            .bearing(90f)         // Sets the orientation of the camera to east
+//            .tilt(30f)            // Sets the tilt of the camera to 30 degrees
+//            .build()              // Creates a CameraPosition from the builder
+//        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.fragment_maps, container, false)
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+//        mapFragment?.getMapAsync(callback)
+//    }
+//}
