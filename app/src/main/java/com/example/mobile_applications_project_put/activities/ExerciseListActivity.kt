0a172a233a4517a3b505aca1e2c4f3b222053340
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2023-06-07 13:46:09 +0200

    fix-text

--- app/src/main/java/com/example/mobile_applications_project_put/activities/ExerciseListActivity.kt
+++ app/src/main/java/com/example/mobile_applications_project_put/activities/ExerciseListActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mobile_applications_project_put.R
 
-class ExcersiceListActivity : AppCompatActivity() {
+class ExerciseListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_excersice_list)
