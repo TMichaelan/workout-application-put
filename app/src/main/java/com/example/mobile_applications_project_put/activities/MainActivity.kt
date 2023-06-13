faeabb77a4b64351648be4a4fc7d03d4364a7bd5
Joramba <jorkabozko@gmail.com>
2023-06-13 13:53:39 +0200

    feat: add loading for workouts

--- app/src/main/java/com/example/mobile_applications_project_put/activities/MainActivity.kt
+++ app/src/main/java/com/example/mobile_applications_project_put/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import com.example.mobile_applications_project_put.R
@@ -16,11 +17,13 @@ import com.google.android.material.tabs.TabLayoutMediator
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
 //        val localWorkoutListActivity: TextView = binding.tvCheckWorkouts
 //        localWorkoutListActivity.setOnClickListener {
