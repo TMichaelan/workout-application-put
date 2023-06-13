4a5cbf9539e6b231c112c0b27270e7d2f450871f
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2023-06-13 14:20:31 +0200

    last-fix

--- app/src/main/java/com/example/mobile_applications_project_put/retrofit/ExcerciseAPI.kt
+++ app/src/main/java/com/example/mobile_applications_project_put/retrofit/ExcerciseAPI.kt
@@ -11,33 +11,33 @@ import retrofit2.http.Path
 interface ExcerciseAPI {
 
     //body parts like back, chest, ...
-    @Headers("X-RapidAPI-Key:2de8606b44mshb36972bfbefdb1ap1d24b6jsn6025ac315a9b")
+    @Headers("X-RapidAPI-Key:d0c56e12ddmsh131696085ff6ffcp133fc5jsnbfc47bbe6422")
     @GET("bodyPartList")
     fun getBodyPartsList(): Call<BodyPartsList>
 
     //Doesn't work because of the API, too much data to load
-    @Headers("X-RapidAPI-Key:2de8606b44mshb36972bfbefdb1ap1d24b6jsn6025ac315a9b")
+    @Headers("X-RapidAPI-Key:d0c56e12ddmsh131696085ff6ffcp133fc5jsnbfc47bbe6422")
     @GET("exercises")
     fun getAllExercisesList(): Call<BodyPartsList>
 
-    @Headers("X-RapidAPI-Key:2de8606b44mshb36972bfbefdb1ap1d24b6jsn6025ac315a9b")
+    @Headers("X-RapidAPI-Key:d0c56e12ddmsh131696085ff6ffcp133fc5jsnbfc47bbe6422")
     @GET("bodyPart/{bodyPart}")
     fun getBodyPart(@Path("bodyPart") bodyPart: String): Call<BodyPartExcerciseList>
 
-    @Headers("X-RapidAPI-Key:2de8606b44mshb36972bfbefdb1ap1d24b6jsn6025ac315a9b")
+    @Headers("X-RapidAPI-Key:d0c56e12ddmsh131696085ff6ffcp133fc5jsnbfc47bbe6422")
     @GET("exercise/{exerciseId}")
     fun getExerciseIdById(@Path("exerciseId") exerciseId: String): Call<ExerciseItem>
 
-    @Headers("X-RapidAPI-Key:2de8606b44mshb36972bfbefdb1ap1d24b6jsn6025ac315a9b")
+    @Headers("X-RapidAPI-Key:d0c56e12ddmsh131696085ff6ffcp133fc5jsnbfc47bbe6422")
     @GET("name/{exerciseName}")
     fun getExerciseListByName(@Path("exerciseName") exerciseId: String): Call<BodyPartExcerciseList>
 
     //targets like: abs, biceps, triceps, ...
-    @Headers("X-RapidAPI-Key:2de8606b44mshb36972bfbefdb1ap1d24b6jsn6025ac315a9b")
+    @Headers("X-RapidAPI-Key:d0c56e12ddmsh131696085ff6ffcp133fc5jsnbfc47bbe6422")
     @GET("targetList")
     fun getTargetPartsList(): Call<BodyPartsList>
 
-    @Headers("X-RapidAPI-Key:2de8606b44mshb36972bfbefdb1ap1d24b6jsn6025ac315a9b")
+    @Headers("X-RapidAPI-Key:d0c56e12ddmsh131696085ff6ffcp133fc5jsnbfc47bbe6422")
     @GET("target/{target}")
     fun getExerciseListByTarget(@Path("target") exerciseId: String): Call<BodyPartExcerciseList>
 }
