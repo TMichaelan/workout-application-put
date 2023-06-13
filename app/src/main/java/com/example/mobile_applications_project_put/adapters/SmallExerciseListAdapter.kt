bc602e13c4155f7b75276ddd249978a33741fa50
Joramba <jorkabozko@gmail.com>
2023-06-13 12:12:56 +0200

    feat: add small loading spinner in home fragment

--- app/src/main/java/com/example/mobile_applications_project_put/adapters/SmallExerciseListAdapter.kt
+++ app/src/main/java/com/example/mobile_applications_project_put/adapters/SmallExerciseListAdapter.kt
@@ -1,12 +1,18 @@
 package com.example.mobile_applications_project_put.adapters
 
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.mobile_applications_project_put.R
 import com.example.mobile_applications_project_put.db.entities.Exercise
 import com.example.mobile_applications_project_put.fragments.HomeFragment
@@ -33,15 +39,35 @@ class SmallExerciseListAdapter (
             val currentExercise = exerciseList[position]
 
             if (!internet) {
-
-                Glide.with(holder.imageId)
-                    .asBitmap()
-                    .load(R.drawable.no_inet)
-                    .into(holder.imageId)
+                holder.progressBar.visibility = View.GONE // Скрыть прогресс-бар
+                holder.imageId.setImageResource(R.drawable.no_inet) // Установить изображение "No Internet"
             } else {
+                holder.progressBar.visibility = View.VISIBLE // Показать прогресс-бар
                 Glide.with(holder.imageId)
                     .asBitmap()
                     .load(currentExercise.gifUrl)
+                    .listener(object : RequestListener<Bitmap> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Bitmap?>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            holder.progressBar.visibility = View.GONE // Скрыть прогресс-бар в случае ошибки загрузки
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Bitmap?,
+                            model: Any?,
+                            target: Target<Bitmap?>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            holder.progressBar.visibility = View.GONE // Скрыть прогресс-бар после успешной загрузки
+                            return false
+                        }
+                    })
                     .into(holder.imageId)
             }
             holder.name.text = currentExercise.name.capitalize()
@@ -61,6 +87,7 @@ class SmallExerciseListAdapter (
         ) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
             val imageId: ImageView = itemView.findViewById(R.id.img_body_part)
             val name: TextView = itemView.findViewById(R.id.tv_body_part)
+            val progressBar: ProgressBar = itemView.findViewById(R.id.small_progress_bar)
 
             init {
                 itemView.setOnClickListener(this)
@@ -70,7 +97,4 @@ class SmallExerciseListAdapter (
                 listener.onItemClick(exerciseList[adapterPosition])
             }
         }
-
-
-
     }
